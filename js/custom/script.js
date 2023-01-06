44780955f7fc43671d533514014b371edb231636
cajunlei <92667767+cajunlei@users.noreply.github.com>
2023-01-06 09:43:44 +0000

    细节样式优化； Updated By Github Actions

--- js/custom/script.js
+++ js/custom/script.js
@@ -1,3 +1,34 @@
+// 作者卡片问好start
+function authorInfoSayhi() {
+  // 获取时间
+  var getTimeState = () => {
+    // 获取当前时间
+    var timeNow = new Date();
+    // 获取当前小时
+    var hours = timeNow.getHours();
+    // 设置默认文字
+    var text = ``;
+    // 判断当前时间段
+    if (hours >= 0 && hours <= 5) {
+      text = `晚安`;
+    } else if (hours > 5 && hours <= 10) {
+      text = `早上好`;
+    } else if (hours > 10 && hours <= 14) {
+      text = `中午好`;
+    } else if (hours > 14 && hours <= 18) {
+      text = `下午好`;
+    } else if (hours > 18 && hours <= 24) {
+      text = `晚上好`;
+    }
+    return text;
+  };
+  if (document.querySelector('#author-info__sayhi')) {
+    document.getElementById("author-info__sayhi").innerHTML = getTimeState() + "！我是";
+  }
+};
+authorInfoSayhi();
+// 作者卡片问好end
+
 // 切换热评start
 function switchCommentBarrage() {
   let flag = window.localStorage.getItem('commentBarrageDisplay') // undefined || false
